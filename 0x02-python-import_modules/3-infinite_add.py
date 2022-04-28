702fb6049d9990c57e4b81506c0ddf9b921ed1c9
MatiasMtz <4442@holbertonschool.com>
2022-04-28 15:39:37 +0000

    program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,9 +4,6 @@ if __name__ == "__main__":
     add = 0
     if len(sys.argv) == 1:
         print("0")
-    #elif len(sys.argv) == 2:
-    #    number = int(sys.argv[1])
-    #    print(f"{number:d}")
     else:
         for count in range(1, len(sys.argv)):
             add += int(sys.argv[count])

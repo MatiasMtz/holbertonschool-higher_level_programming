f9b3139f7e15bb0701ab1b5ec3b6918224d001bf
MatiasMtz <4442@holbertonschool.com>
2022-04-28 15:12:10 +0000

    program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print(f"{arg_count - 1} arguments.")
     elif arg_count == 2:
         print(f"{arg_count - 1} argument:")
-    else: 
+    else:
         print(f"{arg_count - 1} arguments:")
         for count in range(1, arg_count):
             print(f"{count}: {sys.argv[count]}")
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+count = 1
+while count < len(argv):
+    print(ZZ)

46790b38d6ee1ae3bc6effa931a2c0751b940d04
MatiasMtz <4442@holbertonschool.com>
2022-05-04 23:38:44 +0000

    refresh

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def updatZZe_dictionary(a_dictionary, key, value):
-        a_dictionary[key] = value
-        return a_dictionary
+def update_dictionary(a_dictionary, key, value):
+        a_dictionary['key'] = 'value'
+            a_dictionary[key] = value
+                return a_dictionary

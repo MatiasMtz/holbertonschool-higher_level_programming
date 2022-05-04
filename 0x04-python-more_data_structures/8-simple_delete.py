7ec05a6e162f02f8dd329eee25a6d3801f9c566a
MatiasMtz <4442@holbertonschool.com>
2022-05-04 23:45:21 +0000

    refresh

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,4 +2,4 @@
 def simple_delete(a_dictionary, key=""):
         if key in a_dictionary:
                     del a_dictionary[key]
-                        return a_dictionary
+                    return a_dictionary

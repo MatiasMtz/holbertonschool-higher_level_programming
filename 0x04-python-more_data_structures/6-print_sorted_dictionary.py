1d35c4a5da7f18cb0cab139b5e563026567bdcbd
MatiasMtz <4442@holbertonschool.com>
2022-05-04 23:35:35 +0000

    refresh

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    [print(f"{i}: {a_dictionary}" for i in sorted(a_dictionary)]
+    for key in sorted(a_dictionary):
+        print(f"{key}: {a_dictionary[key]}")

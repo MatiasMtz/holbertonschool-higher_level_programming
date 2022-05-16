e8ef059de4930b0fadc5513aeecf1d28c9d2574e
MatiasMtz <4442@holbertonschool.com>
2022-05-16 15:12:06 +0000

    refresh

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print(f"{value:d}")
+        print("{:d}".format(value))
         return True
     except(ValueError, TypeError):
         return False

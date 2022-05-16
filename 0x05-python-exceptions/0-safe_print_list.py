cd687412f5dcfba78872451a23d0b9e933ca50fc
MatiasMtz <4442@holbertonschool.com>
2022-05-16 13:53:15 +0000

    function that prints an integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[count]}", end="")
             count = count + 1
-        except:
+        except IndexError:
             break
     print()
     return(count)
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,7 @@
 def safe_print_integer(value):
+
+    try:
+        print(f"{value:d}")
+        return True
+    except(ValueError, TypeError):
+        return False

9bc3c51663d0e9215945cc2be0f4d0c6d1e4cd25
MatiasMtz <4442@holbertonschool.com>
2022-05-16 15:05:26 +0000

    function that prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,7 @@ def safe_print_integer(value):
 
     try:
         print(f"{value:d}")
-        return True
-    except(ValueError):
+    except(ValueError, TypeError):
         return False
+    else:
+        return True

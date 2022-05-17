b85a40407150dbfd9a6d21bfc1cdf6030b331757
MatiasMtz <4442@holbertonschool.com>
2022-05-17 21:37:58 +0000

    position attribute added

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""Square class: defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes private square size attribute and position attribute
+        Args:
+        size (int): the size of the square once instance is created
+        position (int): position where the square is going to be placed"""
+
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        """Initializes public square area attribute"""
+
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Initializes square size return"""
+        return (self.__size)
+
+    @property
+    def position(self):
+        """Initializes square position"""
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        """Initializes square size attribute
+        Args:
+        value (int): the size of a size of the square"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """Initializes square position attribute
+        Args:
+        value (int): the position of the square"""
+
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        """Initializes square printing"""
+
+        if self.__size == 0:
+            print()
+        else:
+            for position1 in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for position2 in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()

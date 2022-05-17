d1b6c7e4673951f98e0f75618bad730d30d35ed0
MatiasMtz <4442@holbertonschool.com>
2022-05-17 20:23:00 +0000

    square class with size, except and area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square class: defines a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes private square size attribute
+        Args:
+        size (int): the size of the square once instance is created"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """Initializes public square area attribute"""
+
+        return (self.__size * self.__size)

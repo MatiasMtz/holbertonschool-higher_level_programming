cd556c64719de68055c8d3d55f95d87ba3058aad
MatiasMtz <4442@holbertonschool.com>
2022-05-18 21:49:40 +0000

    9???

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -4,8 +4,8 @@
 
 class Rectangle:
     """Represents a rectangle"""
-    number_of_instances = 0
     print_symbol = "#"
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializes Rectangle instance attributes

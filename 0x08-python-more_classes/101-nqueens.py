2d7d030e15012a07ac07bbd67c8740e95155c715
MatiasMtz <4442@holbertonschool.com>
2022-05-18 20:27:21 +0000

    bNqueens solved

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""N-Queen puzzle using backtracking"""
+
+
+def safeSpot(queenPosition, queenN):
+    """Safe spot checker
+    Args:
+        queenPosition: Array with queens positions
+        queenN: Amount of queens"""
+    for i in range(queenN):
+        if queenPosition[i] == queenPosition[queenN]:
+            return False
+        if abs(queenPosition[i] - queenPosition[queenN]) == abs(i - queenN):
+            return False
+    return True
+
+def locateQueens(queenPosition, queenNumber):
+    """Prints the list with the Queens positions
+    Args:
+        queenPosition: Array with queens positions
+        queenNumber: amount of queens"""
+    queens = []
+    for i in range(queenNumber):
+        queens.append([i, queenPosition[i]])
+    print(queens)
+
+def Queen(queenPosition, queenNumber):
+    """Backtracking algorithm
+    Args:
+        queenPosition: Array with queens positions
+        queenNumber: amount of queens"""
+    if queenNumber is len(queenPosition):
+        locateQueens(queenPosition, queenNumber)
+        return
+    queenPosition[queenNumber] = -1
+    while((queenPosition[queenNumber] < len(queenPosition) - 1)):
+        queenPosition[queenNumber] += 1
+        if safeSpot(queenPosition, queenNumber) is True:
+            if queenNumber is not len(queenPosition):
+                Queen(queenPosition, queenNumber + 1)
+
+def solveNQueen(size):
+    """Function that applies backtracking algorithm
+    Args:
+        size: size of the chessboard"""
+    queenPosition = [-1 for i in range(size)]
+    Queen(queenPosition, 0)
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        size = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        sys.exit(1)
+    if size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solveNQueen(size)
---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+"""Rectangle class: defines a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initializes Rectangle instance attributes
+        Args:
+        width (int): width of Rectangle once instance is created
+        height (int): height of Rectangle once instance is created"""
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Static method that returns the biggest rectangle based on area
+        Args:
+        rect_1 (Rectangle): first object to compare
+        rect_2 (Rectangle): second object to compare"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        rect1Area = rect_1.area()
+        rect2Area = rect_2.area()
+        if rect1Area == rect2Area or rect1Area > rect2Area:
+            return rect_1
+        if rect1Area < rect2Area:
+            return rect_2
+
+    @property
+    def width(self):
+        """Defines a private instance: Width"""
+        return (self.__width)
+
+    @property
+    def height(self):
+        """Defines a private instance: Height"""
+        return (self.__height)
+
+    @width.setter
+    def width(self, value):
+        """Rectangle width attribute setter
+        Args:
+        value (int): Width of the Rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Rectangle height attribute setter
+        Args:
+        value (int): Height of the Rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Public instance method that returns the Rectangle area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Public instance method that returns the Rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Method to represent Rectangle class object as a string"""
+        if self.width == 0 or self.__height == 0:
+            return ""
+        if not isinstance(self.print_symbol, str):
+            self.print_symbol = str(self.print_symbol)
+        rectangle = []
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle.append(self.print_symbol)
+            if i < (self.__height - 1):
+                rectangle.append("\n")
+        printRectangle = "".join(rectangle)
+        return (printRectangle)
+
+    def __repr__(self):
+        """Method to represent Rectangle class object as machine language"""
+        return f'(Rectangle({self.__width}, {self.__height}))'
+
+    def __del__(self):
+        """Method to print when an instance of Rectangle is deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @classmethod
+    def square(cls, size=0):
+        """Method that returns a new Rectangle instance"""
+        return cls(size, size)

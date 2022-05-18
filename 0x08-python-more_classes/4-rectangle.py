45026d2dce5db713ce65985a65c7c83615dca749
MatiasMtz <4442@holbertonschool.com>
2022-05-18 21:23:34 +0000

    fix f -> format in task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -73,4 +73,4 @@ class Rectangle:
 
     def __repr__(self):
         """Method to represent Rectangle class object as machine language"""
-        return f'(Rectangle({self.__width}, {self.__height}))'
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)

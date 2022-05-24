fbc6831a04d7fef354d949b63738a29c92c0e5e9
MatiasMtz <4442@holbertonschool.com>
2022-05-24 18:08:42 +0000

    task 101

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+globalCounter = 0
+def magic_string():
+    global globalCounter = globalCounter + 1
+    return ("BestSchool" * globvar)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Locked class"""
+
+
+class LockedClass:
+    """Class that just allow attribute called first_name"""
+    __slots__ = ["first_name"]

407bf810cc88afead0be92f12e173948e69a9fa1
MatiasMtz <4442@holbertonschool.com>
2022-07-05 23:02:09 +0000

    fixed upper

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Lists all records with a score >= 10 in the table second_table
-SELECT score, name from second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

1840358670e1d674b7478e3ddf019948820988a1
MatiasMtz <4442@holbertonschool.com>
2022-07-05 22:57:31 +0000

    Tasks 10-16 mysql

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Removes all records with a score <= 5 in the table second_table
+DELETE from second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

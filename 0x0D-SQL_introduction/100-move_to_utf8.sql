a027cab0a628283ab133ed91b966d3ab90220977
MatiasMtz <4442@holbertonschool.com>
2022-07-05 23:33:41 +0000

    fix 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
--- converts hbtn_0c_0 database to UTF8
-USE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Converts hbtn_0c_0 database to UTF8
+USE hbtn_0c_0
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

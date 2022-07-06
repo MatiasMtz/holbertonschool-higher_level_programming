5491596e73d01b8ce1a0d171121a7dc0a182fe26
MatiasMtz <4442@holbertonschool.com>
2022-07-06 21:15:38 +0000

    Tasks 11 - 16

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id.genre_id;
---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;

00a1a1c979ba319f470a4507eed00d1f609f2289
MatiasMtz <4442@holbertonschool.com>
2022-07-06 21:37:23 +0000

    fix 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,5 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id.genre_id;
+LEFT JOIN tv_genres	ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

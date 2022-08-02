d324400e01e00f332b99b1b5c7293398260229b8
MatiasMtz <4442@holbertonschool.com>
2022-08-02 18:49:59 +0000

    task 8 without reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 // function that returns the reversed version of a list
 exports.esrever = function (list) {
-  return list.reverse();
+  let end = list.length;
+  let start = 0;
+  const newList = [];
+  for (start = 0; start < list.length; start++) {
+    newList[start] = list[end - 1];
+    end--;
+  }
+  return newList;
 };

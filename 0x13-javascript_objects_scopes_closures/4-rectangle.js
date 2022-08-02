d46433771f43ea5ebda2ae6f4e0e4cc5f884acd0
MatiasMtz <4442@holbertonschool.com>
2022-08-02 18:12:23 +0000

    task 4 and semistandard fixes

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,6 +13,17 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 }
 
 module.exports = Rectangle;

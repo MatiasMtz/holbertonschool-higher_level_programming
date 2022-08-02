68e7b05b7beb586900cb041ab9bb45fed31d17b6
MatiasMtz <4442@holbertonschool.com>
2022-08-02 19:15:32 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // function that converts a number from base 10 to another base passed as argument
 exports.converter = function (base) {
-	return function (number) {
-		return number.toString(base);
-	};
+  return function (number) {
+    return number.toString(base);
+  };
 };
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 // script that imports an array and computes a new array
 const list = require('./100-data').list;
 const newList = list.map(function (number, index) {
-	return (number * index);
+  return (number * index);
 });
 console.log(list);
 console.log(newList);

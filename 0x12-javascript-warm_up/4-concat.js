188d8023a4275a193f0277db715ea2f8549e6087
MatiasMtz <4442@holbertonschool.com>
2022-08-02 00:10:43 +0000

    tasks 4,5

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// script that prints two arguments passed to it, in the following format: "is"
+const { argv } = require('process');
+let first;
+let second;
+
+if (argv[2] === undefined) {
+	first = 'undefined';
+} else {
+	first = argv[2];
+}
+if (argv[3] === undefined) {
+	second = 'undefined';
+} else {
+	second = argv[3];
+}
+console.log(`${first} is ${second}`);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
+const { argv } = require('process');
+
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+	console.log('Not a number');
+} else {
+	console.log(`My number: ${argv[2]}`);
+}

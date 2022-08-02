043407994fcfc67702a1a60b40227976b85ff4b1
MatiasMtz <4442@holbertonschool.com>
2022-08-02 00:08:46 +0000

    tasks 2, 3

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Read an argument and print it
+const { argv } = require('process');
+const argc = argv.length - 2;
+
+if (argc === 0) {
+	console.log('No argument');
+} else if (argc === 1) {
+	console.log('Argument found');
+} else if (argc > 1) {
+	console.log('Arguments found');
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints the first argument passed to it
+const { argv } = require('process');
+
+if (argv[2] === undefined) {
+	console.log('No argument');
+} else {
+	console.log(argv[2]);
+}

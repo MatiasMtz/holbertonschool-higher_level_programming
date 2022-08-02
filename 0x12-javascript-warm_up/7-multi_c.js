d46a42ad54718fe5a19f4d1e58719299cee21d19
MatiasMtz <4442@holbertonschool.com>
2022-08-02 00:24:46 +0000

    tasks 7,8,9 and 10

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints 3 lines using an array of string and a loop
+
+const lang = 'C is fun';
+
+if (isNaN(process.argv[2])) {
+	console.log('Missing number of occurrences');
+} else {
+	for (let i = 0; i < parseInt(process.argv[2]); i++) {
+		console.log(lang);
+	}
+}

3f5d7602f3609e19c7acf4915cf1586cc270e981
MatiasMtz <4442@holbertonschool.com>
2022-08-02 19:15:13 +0000

    task 100

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that imports an array and computes a new array
+const list = require('./100-data').list;
+const newList = list.map(function (number, index) {
+	return (number * index);
+});
+console.log(list);
+console.log(newList);

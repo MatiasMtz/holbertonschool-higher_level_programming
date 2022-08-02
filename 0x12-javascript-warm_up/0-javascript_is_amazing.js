d65ec0dccf477a5722069757fa7a7d97d1764895
MatiasMtz <4442@holbertonschool.com>
2022-08-02 00:02:14 +0000

    tasks 0-1 and readme

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// script that prints “JavaScript is amazing”
+const myVar = "JavaScript is amazing";
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// script that prints 3 lines
+console.log('C is fun\nPython is cool\nJavaScript is amazing');

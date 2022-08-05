b9208eac739b06f5ef44b7ad94a1d771887fad82
MatiasMtz <4442@holbertonschool.com>
2022-08-05 22:13:45 +0000

    pycodestyle fixes

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-""""""
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    """"""
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,8 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-	"""links to the MySQL table states"""
-	__tablename__ = 'states'
-	id = Column(Integer, primary_key=True)
-	name = Column(String(128), nullable=False)
+    """links to the MySQL table states"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)

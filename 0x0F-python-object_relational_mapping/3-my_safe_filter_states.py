b4e2ba0c03fe4c943126643daeb73d4b8c70a7a7
MatiasMtz <4442@holbertonschool.com>
2022-08-05 21:09:35 +0000

    task 4 and pycode fix

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
+
+if __name__ == "__main__":
+    """Access to db and get the values"""
+    from sys import argv
+    import MySQLdb
+
+    # Connecting to a MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # Getting a Cursor in MySQL Python
+    cur = db.cursor()
+
+    # Executing MySQL Queries in Python
+    cur.execute("SELECT * FROM states WHERE name='{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+
+    # Obtaining Query Results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Clean up
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    """Access to db and get the values"""
+    from sys import argv
+    import MySQLdb
+
+    # Connecting to a MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # Getting a Cursor in MySQL Python
+    cur = db.cursor()
+
+    # Executing MySQL Queries in Python
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                 JOIN states ON cities.state_id=states.id \
+                 ORDER BY cities.id ASC")
+
+    # Obtaining Query Results
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Clean up
+    cur.close()
+    db.close()

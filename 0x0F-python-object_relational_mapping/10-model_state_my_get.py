ddd7c248a7bcd5813a7f74fc2bf81b7ba74e05a6
MatiasMtz <4442@holbertonschool.com>
2022-08-05 23:00:57 +0000

    task 11 and doc fixes

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that prints the first State object from the database hbtn_0e_6_usa"""
+"""script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
     """Access to db and get the values"""
--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa"""
+"""script that adds the State object “Louisiana” to the database
+hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
     """Access to db and get the values"""
@@ -17,6 +17,10 @@ if __name__ == "__main__":
     Session = sessionmaker(engine)
     session = Session()
 
-    # Obtaining Query Results
-    for result in session.query(State).filter(State.name.contains("a")):
-        print("{}: {}".format(result.id, result.name))
+    # Add a new record
+    newState = State(name="Louisiana")
+    session.add(newState)
+    session.commit()
+
+    # Print states.id after creation
+    print("{}".format(newState.id))

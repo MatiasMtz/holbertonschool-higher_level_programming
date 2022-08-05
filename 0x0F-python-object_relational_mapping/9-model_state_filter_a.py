71c58617b72529898a415c85dd74116c69938842
MatiasMtz <4442@holbertonschool.com>
2022-08-05 22:49:09 +0000

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that prints the first State object from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    """Access to db and get the values"""
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    # Engine creation
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Session initializer (automatically commits and closes sessions)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    # Obtaining Query Results
+    for result in session.query(State).filter(State.name.contains("a")):
+        print("{}: {}".format(result.id, result.name))

354f77be94c3f3d77a8d4b0fc330231218de24d6
MatiasMtz <4442@holbertonschool.com>
2022-08-05 23:10:24 +0000

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that adds the State object “Louisiana” to the database
+hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    """Access to db and get the values"""
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    # Engine creation
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Session initializer (automatically commits and closes sessions)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    # Update record
+    for query in session.query(State).filter(State.name.contains('a')):
+        session.delete(query)
+    session.commit()
+
+    # Close session
+    session.close()

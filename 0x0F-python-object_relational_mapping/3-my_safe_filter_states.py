ed34be02aaf20c10bb4041518e6d7e3baa3dd314
MatiasMtz <4442@holbertonschool.com>
2022-08-07 20:00:27 +0000

    fix 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
     # Obtaining Query Results
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
 
     # Clean up
     cur.close()

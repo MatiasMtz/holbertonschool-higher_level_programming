252174036d7154baf84d128b86257c48b0ff670e
MatiasMtz <4442@holbertonschool.com>
2022-09-12 19:11:31 +0000

    script that display the status code of GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that display the status code of a GET request
+const axios = require('axios');
+const args = process.argv.slice(2);
+
+axios.get(args[0])
+  .then(function (response) {
+    console.log('code: ', response.status);
+  })
+  .catch(error => {
+    console.log('code:', error.response.status);
+  });

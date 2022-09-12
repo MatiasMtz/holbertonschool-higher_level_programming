c3e89fbb6f8c9d3d3e8dd09da22e28c57847adec
MatiasMtz <4442@holbertonschool.com>
2022-09-12 19:18:11 +0000

    fixed output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const axios = require('axios');
 const args = process.argv.slice(2);
 
 axios.get(args[0])
-  .then(function (response) {
-    console.log('code: ', response.status);
+  .then(response => {
+    console.log('code:', response.status);
   })
-  .catch(function (error) {
-    console.log('code: ', error.response.status);
+  .catch(error => {
+    console.log('code:', error.response.status);
   });

229f3bb0133b4cac8a54631bd261982b1be8b483
MatiasMtz <4442@holbertonschool.com>
2022-09-12 18:45:42 +0000

    reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+const fs = require('fs');
+var args = process.argv.slice(2);
+
+fs.readFile(args[0], 'utf8', (error, data) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+	console.log(data);
+})

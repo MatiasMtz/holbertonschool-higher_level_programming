0cff23d782ce49d6ddefee6bd85c5b02ea1b73ac
MatiasMtz <4442@holbertonschool.com>
2022-09-19 18:31:00 +0000

    toggles the class of an element when clicking

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+// script that toggles the class of the <header> element when 
+// the user clicks on the tag DIV#toggle_header:
+$('DIV#toggle_header').click(function () {
+	const color = $('header').hasClass('red');
+	if (!color) {
+		$('header').toggleClass('red');
+	} else {
+		$('header').toggleClass('green');
+	}
+});

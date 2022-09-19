0bc8cc4bf286f74776171f7258539a4a89a3829e
MatiasMtz <4442@holbertonschool.com>
2022-09-19 18:36:14 +0000

    updates header tag text from html

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// script that updates the text of the <header> element to New Header!!!
+// when the user clicks on DIV#update_header
+$('DIV#update_header').click(function () {
+	$('header').text('New Header!!!');
+});

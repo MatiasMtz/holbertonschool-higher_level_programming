5efaa99088e9521f0351d1534a322460d08b13ea
MatiasMtz <4442@holbertonschool.com>
2022-09-19 18:38:27 +0000

    fetches data from api

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,5 @@
+// script that fetches the character name from this 
+// URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+	$('DIV#character').text(data.name);
+});
